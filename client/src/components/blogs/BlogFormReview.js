bc52d261377a146b270b6f4fff511d3894952bd5
milesros <miles.rostami@gmail.com>
2018-05-15 13:06:00 -0400

    completed file upload to s3 bucket functionality

--- client/src/components/blogs/BlogFormReview.js
+++ client/src/components/blogs/BlogFormReview.js
@@ -7,9 +7,8 @@ import { withRouter } from 'react-router-dom';
 import * as actions from '../../actions';
 
 class BlogFormReview extends Component {
-  state={
-    file: null
-  }
+  state = { file: null };
+
   renderFields() {
     const { formValues } = this.props;
 
@@ -50,11 +49,8 @@ class BlogFormReview extends Component {
     submitBlog(formValues, this.state.file, history);
   }
 
-  onFileChange = (event) =>{
-    this.setState({
-      file: event.target.files
-    })
-    console.log(event.target.files);
+  onFileChange(event) {
+    this.setState({ file: event.target.files[0] });
   }
 
   render() {
@@ -62,12 +58,14 @@ class BlogFormReview extends Component {
       <form onSubmit={this.onSubmit.bind(this)}>
         <h5>Please confirm your entries</h5>
         {this.renderFields()}
-        <h5>Add an image</h5>
-        <input 
-        type="file" 
-        accept="image/*" 
-        onChange={this.onFileChange}
+
+        <h5>Add An Image</h5>
+        <input
+          onChange={this.onFileChange.bind(this)}
+          type="file"
+          accept="image/*"
         />
+
         {this.renderButtons()}
       </form>
     );
